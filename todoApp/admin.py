f85b019a9f0a7390c4f621243da3f57a464300d3
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-11 11:38:14 +0600

    created models and permissions

--- todoApp/admin.py
+++ todoApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from todoApp.models import TodoItem,UserRole
+
+
+class AdminTask(admin.ModelAdmin):
+    list_display=['title','description','created_by','created_at','updated_at','delivery_date']
+admin.site.register(TodoItem,AdminTask)
+
+class AdminRole(admin.ModelAdmin):
+    list_display=['user','role']
+admin.site.register(UserRole,AdminRole)
--- todoApp/models.py
+++ todoApp/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserRole(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='users')
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('staff', 'Staff'),
+        ('other', 'Other')
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='other')
+    def __str__(self):
+        return self.role
+
+
+from django.db import models
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_by = models.ForeignKey(UserRole, on_delete=models.CASCADE,related_name='userRole')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    delivery_date = models.DateField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.title
+    
