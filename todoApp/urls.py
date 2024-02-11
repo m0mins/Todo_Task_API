29ee9b30a63ef9bcea1f828f78d920d44394ed35
m0mins <momin.pstu.cse11.bd@gmail.com>
2024-02-11 11:50:41 +0600

    updated apps urls

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -1,10 +1,28 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+#from .views import ToDoTaskListView,ToDoTaskDetailView
+from .views import TodoItemListCreate,TodoItemRetrieveUpdateDestroy
+from .views import UserRegistrationAPIView,UserLoginAPIView
+from rest_framework_simplejwt.views import TokenVerifyView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 #router = DefaultRouter()
 #router.register(r'todoitems', ToDoTaskAPIView,basename='todoitem')
 
 urlpatterns = [
-  
+    #path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('register/', UserRegistrationAPIView.as_view(), name='user-registration'),
+    path('login/', UserLoginAPIView.as_view(), name='user-login'),
+    path('todos/', TodoItemListCreate.as_view(), name='todo-list'),
+    path('todos/<int:pk>/', TodoItemRetrieveUpdateDestroy.as_view(), name='todo-detail'),
 
 
 ]
